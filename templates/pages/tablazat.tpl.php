c7dded037a203a66e915414881aff4117b209097
nnarozsnik <nnarozsnik@gmail.com>
2026-05-14 17:42:37 +0200

    np adatbázis kapcsolat

--- templates/pages/tablazat.tpl.php
+++ templates/pages/tablazat.tpl.php
@@ -1,33 +1,18 @@
-<table>
-    <caption>5. gyakorlat 1. feladat:<br>TÁBLÁZAT</caption>
-    <tr>
-        <th>Employee</th>
-        <th>Salary</th>
-        <th>Bonus</th>
-        <th>Supervisor</th>
-    </tr>
-    <tr>
-        <td>Stephen C. Cox</td>
-        <td>$300</td>
-        <td>$50</td>
-        <td>Bob</td>
-    </tr>
-    <tr>
-        <td>Josephin Tan</td>
-        <td>$150</td>
-        <td>-</td>
-        <td>Annie</td>
-    </tr>
-    <tr>
-        <td>Joyce Ming</td>
-        <td>$200</td>
-        <td>$35</td>
-        <td>Andy</td>
-    </tr>
-    <tr>
-        <td>James A. Pentel</td>
-        <td>$175</td>
-        <td>$25</td>
-        <td>Annie</td>
-    </tr>
-</table>
+<?php
+$dbh = new PDO('mysql:host=localhost;dbname=gyakorlat7','root','');
+
+$stmt = $dbh->query("SELECT * FROM np");
+
+echo "<h2>Nemzeti parkok</h2>";
+echo "<table border='1'>";
+echo "<tr><th>ID</th><th>Név</th></tr>";
+
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo "<tr>";
+    echo "<td>".$row['id']."</td>";
+    echo "<td>".$row['nev']."</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+?>
